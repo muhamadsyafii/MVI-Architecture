e378ce04468aaa36a0a9210600330c066d18bc00
Muhamad Syafii <muhamadsyafii@Syafii-MacBook-Pro.local>
2021-08-26 08:14:39 +0700

    add permission internet and add class mainviewmodel

---
+++ app/src/main/java/com/syafii/mvi/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.syafii.mvi.ui.main.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.syafii.mvi.data.repository.MainRepository
+import com.syafii.mvi.ui.main.intent.MainIntent
+import com.syafii.mvi.ui.main.viewstate.MainState
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class MainViewModel(private val repository: MainRepository) : ViewModel() {
+
+    val userIntent = Channel<MainIntent>(Channel.UNLIMITED)
+    private val _state = MutableStateFlow<MainState>(MainState.Idle)
+    val state : StateFlow<MainState>
+    get() = _state
+    init {
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+        viewModelScope.launch {
+            userIntent.consumeAsFlow().collect {
+                when(it){
+                    is MainIntent.FetchUser -> fetchUser()
+                }
+            }
+        }
+    }
+
+    private fun fetchUser() {
+        viewModelScope.launch {
+            _state.value = MainState.Loading
+            _state.value = try {
+                MainState.Users(repository.getUsers())
+            } catch (e : Exception){
+                MainState.Error(e.localizedMessage)
+            }
+        }
+    }
+}
